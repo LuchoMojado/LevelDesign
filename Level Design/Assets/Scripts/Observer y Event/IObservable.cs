093791ee85d3e727f5472fd1cfd5b0a15ff1733b
LuchoMojado <bernacchia4597@gmail.com>
2023-10-06 15:11:33 -0300

    f player

--- Level Design/Assets/Scripts/Observer y Event/IObservable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IObservable
-{
-    void Subscribe(IObserver x);
-    void Unsubscribe(IObserver x);
-}
