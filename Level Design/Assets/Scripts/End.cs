8132ce5bfccbbe6783cdd5e8840fae4d944fb567
Maximiliano Pereira <maxidape44@gmail.com>
2023-12-02 14:53:07 -0300

    EndThing
    Sheeesh

--- Level Design/Assets/Scripts/End.cs
+++ Level Design/Assets/Scripts/End.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class End : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string sceneToLoad;
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter(Collision collision)
     {
         
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
     }
 }
